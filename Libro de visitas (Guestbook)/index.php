862b2f525aba63e0c0c924aa8a1630c2790e2c61
Helger <helgerjo123@gmail.com>
2025-04-20 23:07:01 -0500

    adding comments in index.php

--- Libro de visitas (Guestbook)/index.php
+++ Libro de visitas (Guestbook)/index.php
@@ -25,11 +25,16 @@
     <h2>Mensajes anteriores:</h2>
     <div class="mensajes_anteriores">
         <?php 
-        $path = './data/messages.txt';
-            if (file_exists($path)) {
-                $lines = file($path);
-                foreach($lines as $line) {
-                    echo nl2br(htmlspecialchars($line)) . "<br>";
+        $path = './data/messages.txt'; 
+            // file_exists($path) -> Verifica si el archivo donde guardamos los mensajes ya existe. 
+            if (file_exists($path)) { 
+                $lines = file($path); // Carga todas las lineas del archivo en un arreglo (cada linea es un mensaje).
+                
+                //array_reverse() -> Cambia el orden del arreglo para mostrar los mensajes mas recientes primero.
+                foreach(array_reverse($lines) as $line) { 
+                    // htmlspecialchars($line) -> Evita que alguien inyecte codigo HTML malicioso.
+                    // nl2br() -> Convierte los saltos de linea \n en etiquetas <br> para que se vean en el navegador.
+                    echo nl2br(htmlspecialchars($line)) . "<hr>";
                 }
             } else {
                 echo "Aun no hay mensajes";
